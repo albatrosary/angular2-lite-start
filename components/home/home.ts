82736f884850ed42c030503e05da7b6290df15ed
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-04-13 10:39:20 +0900

    first commit

---
+++ components/home/home.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core'
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>My First {{name}} {{1+1}} App</h1>
+    `
+})
+
+export class AppComponent {
+  public name = 'Angular';
+}
+　
