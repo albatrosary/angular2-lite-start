01badde865ec4c705e9d001e7ec29afe65dd0d41
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-06-23 22:07:19 +0900

    change system-config.ts

--- src/system-config.ts
+++ src/system-config.ts
@@ -1,26 +1,56 @@
+/**
+ * System configuration for Angular 2 samples
+ * Adjust as necessary for your application needs.
+ */
 declare var System: any;
 
-// apply the CLI SystemJS configuration.
-System.config({
-  paths: {
-    "node_modules:": "node_modules/",
-    "app/": "bin/scripts/"
-  },
-  browserConfig: {
-    "baseURL": "/"
-  },
-  map: {
-    '@angular': 'node_modules/@angular',
-    'rxjs': 'node_modules/rxjs',
-    'scripts/main': 'scripts/main.js'
-  },
-  packages: {
-    '@angular/core': { main: 'index' },
-    '@angular/common': { main: 'index' },
-    '@angular/compiler': { main: 'index' },
-    '@angular/platform-browser': { main: 'index' },
-    '@angular/platform-browser-dynamic': { main: 'index' },
-    'rxjs': { main: 'Rx' },
-    'components': { main: 'index' }
-  }
-});
+// map tells the System loader where to look for things
+System.packageWithIndex = true;
+
+let map = {
+  'app':                        'app', // 'dist',
+  'scripts/main':               'scripts/main.js',
+  '@angular':                   'node_modules/@angular',
+  'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+  'rxjs':                       'node_modules/rxjs'
+};
+// packages tells the System loader how to load when no filename and/or no extension
+let packages = {
+  'app':                        { main: 'script/main.js',  defaultExtension: 'js' },
+  'rxjs':                       { defaultExtension: 'js' },
+  'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+  'components':                 { main: 'index', defaultExtension: 'js' }
+};
+let ngPackageNames = [
+  'common',
+  'compiler',
+  'core',
+  'forms',
+  'http',
+  'platform-browser',
+  'platform-browser-dynamic',
+  'router',
+  'router-deprecated'
+];
+// Individual files (~300 requests):
+let packIndex = (pkgName) => {
+  packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+}
+// Bundled (~40 requests):
+let packUmd = (pkgName) => {
+  packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+}
+// Most environments should use UMD; some (Karma) need the individual index files
+let setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
+// Add package entries for angular packages
+ngPackageNames.forEach(setPackageConfig);
+let config = {
+  map: map,
+  packages: packages
+};
+
+console.log(config);
+
+System.config(config);
+
