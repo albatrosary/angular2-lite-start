11205b42b5b48810895adeda82d012bafa9e57de
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-07-02 13:07:35 +0900

    rc4

--- src/system-config.ts
+++ src/system-config.ts
@@ -11,14 +11,12 @@ let map = {
   'app':                        'app', // 'dist',
   'scripts/main':               'scripts/main.js',
   '@angular':                   'node_modules/@angular',
-  'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
   'rxjs':                       'node_modules/rxjs'
 };
 // packages tells the System loader how to load when no filename and/or no extension
 let packages = {
   'app':                        { main: 'script/main.js',  defaultExtension: 'js' },
   'rxjs':                       { defaultExtension: 'js' },
-  'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   'components':                 { main: 'index', defaultExtension: 'js' }
 };
 let ngPackageNames = [
