f6f998a0328bcc9af99bc0676130b365ac8f82da
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-08-19 11:48:40 +0900

    add karma

---
+++ src/components/home/home.spec.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { AppComponent } from './home';
+
+describe("TableComponent", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ]
+    });
+  });
+
+  it('component test', async(() => {
+    TestBed.compileComponents().then(() => {
+      // コンポーネントインスタンスの作成 
+      let fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+
+      let element = fixture.debugElement.query(By.css('h1')).nativeElement;
+      expect(element.innerText).toMatch(/My First Angular 2 App/i);
+    });
+  }));
+});
